28982b641e9e525d022343c1f24ca78453644123
jineshkj <jineshkj@gmail.com>
2014-03-12 17:58:56 -0700

    A basic slab allocator

---
+++ Project2/Executive/SlabAllocator.cpp
@@ -0,0 +1,41 @@
+//////////////////////////////////////////////////////////////////////////
+//    SlabAllocator.cpp - test source file for SlabAllocator template   //
+//    version 1.0                                                       //
+//                                                                      //
+//    Language     - C++                                                //
+//    Platform     - Any                                                //
+//    Application  - CIS 687 OOD Project 2                              //
+//    Author       - Jinesh Jayakumar <jineshkj@gmail.com>              //
+//                                                                      //
+//    Jinesh Jayakumar (c) Copyright 2014                               //
+//    All rights are granted to the user except the right               //
+//    to publish, and requires the retention of this notice.            //
+//                                                                      //
+//////////////////////////////////////////////////////////////////////////
+
+#ifdef TEST_SLABALLOCATOR
+
+#include <string>
+#include <iostream>
+
+#include "SlabAllocator.h"
+
+struct testdata
+{
+  int i;
+  std::string s;
+};
+
+void main()
+{
+  SlabAllocator<testdata> all;
+
+  std::cout << "1 : " << (void *)all.alloc() << std::endl;
+  std::cout << "2 : " << (void *)all.alloc() << std::endl;
+  std::cout << "3 : " << (void *)all.alloc() << std::endl;
+  std::cout << "4 : " << (void *)all.alloc() << std::endl;
+  std::cout << "5 : " << (void *)all.alloc() << std::endl;
+  std::cout << "6 : " << (void *)all.alloc() << std::endl;
+}
+
+#endif // TEST_SLABALLOCATOR
---
+++ Project2/Executive/SlabAllocator.h
@@ -0,0 +1,90 @@
+#ifndef OOD_SLABALLOCATOR_H
+#define OOD_SLABALLOCATOR_H
+/////////////////////////////////////////////////////////////////////
+//  SlabAllocator.h - Allocate memory using slabs                  //
+//  ver 1.0                                                        //
+//                                                                 //
+//  Language:      C++                                             //
+//  Platform:      Any                                             //
+//  Application:   CIS 687 Spring 2014 Project 2                   //
+//  Author:        Jinesh Jayakumar, jineshkj@gmail.com            //
+//                                                                 //
+//  Jinesh Jayakumar (c) Copyright 2014                            //
+//  All rights are granted to the user except the right            //
+//  to publish, and requires the retention of this notice.         //
+//                                                                 //
+/////////////////////////////////////////////////////////////////////
+/*
+Module Operations:
+==================
+This module defines the SlabAllocator template that can be used to 
+allocate objects from slabs. The allocator creates and maintains slabs
+that expand based on usage in a power-of-2 fashion. New slabs are added
+to the storage pool and the size of new one is double the size of last 
+one added. First slab always start with size of 1.
+
+Public Interface:
+=================
+SlabAllocator<int> a;
+int * i = a.alloc();
+
+Build Process:
+==============
+Required files
+- SlabAllocator.h
+Build commands (either one)
+- cl /EHsc /DTEST_SLABALLOCATOR SlabAllocator.cpp
+
+Maintenance History:
+=====================
+ver 1.0 : 12 Mar 2014
+- first release
+
+*/
+
+#include <vector>
+
+template <class T>
+class SlabAllocator
+{
+public:
+  SlabAllocator() : _used(0)
+  {
+    _stor.push_back(new T[1]);
+  }
+
+  size_t slot_capacity() const
+  {
+    return (1 << (_stor.size() - 1));
+  }
+
+  T * alloc()
+  {
+    T * item = (_used < slot_capacity()) ? 
+                   (_stor[_stor.size() - 1] + _used) : expand();
+
+#ifdef TEST_SLABALLOCATOR
+    std::cout << "Slot: " << (_stor.size() - 1)  << " Capacity: " << slot_capacity() << " Used: " << _used << std::endl;
+#endif
+
+    _used++;
+
+    return item;
+  }
+
+private:
+  size_t _used;
+  std::vector<T*> _stor;
+
+  T * expand()
+  {
+    T * new_slot = new T[_stor.size() << 1];
+
+    _used = 0;
+    _stor.push_back(new_slot);
+
+    return new_slot;
+  }
+};
+
+#endif // OOD_SLABALLOCATOR_H
