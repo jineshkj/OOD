f1ab35b3f0e3e7d221415042e2521c1c90b17c0d
jineshkj <jineshkj@gmail.com>
2014-05-02 17:43:38 -0700

    Added run.bat and compile.bat

---
+++ Project4/run.bat
@@ -0,0 +1,38 @@
+
+
+@echo off
+
+cls
+echo Going to start Server process
+pause
+@echo on
+start Debug\Server.exe
+
+@echo off
+echo Started the service
+pause
+
+cls
+echo Contents of Downloads/ directory
+dir Downloads /p
+pause
+echo Now going execute Tester application with 2 processes
+pause
+@echo on
+Tester\bin\Debug\Tester.exe 2
+@echo off
+pause
+
+cls
+echo Going to start GUI for testing
+pause
+@echo on
+start /WAIT GuiTester\bin\Debug\GuiTester.exe
+@echo off
+pause
+
+cls
+echo Going to kill the server process.
+pause
+
+taskkill /IM Server.exe
