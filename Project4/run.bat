edcc6b9ac590df829c041865bb843af2b5bc37e8
jineshkj <jineshkj@gmail.com>
2014-05-02 18:05:44 -0700

    Added few more tests to run.bat. Added Downloads/a.txt

--- Project4/run.bat
+++ Project4/run.bat
@@ -13,9 +13,49 @@ echo Started the service
 pause
 
 cls
-echo Contents of Downloads/ directory
+echo Going to list the contents of Downloads/ directory
+pause
 dir Downloads /p
 pause
+
+cls
+del Downloads\a.txt
+echo Going to check if Downloads\a.txt file exists
+pause
+dir Downloads\a.txt
+pause
+echo Now going to transfer a.txt from current directory
+pause
+@echo on
+Debug\Client.exe 1 1 PutFile a.txt
+@echo off
+pause
+echo Going to check to see if Downloads\a.txt exists
+pause
+dir Downloads\a.txt
+pause
+
+cls
+echo Going to search for the string "hello world" using Client.exe using 1 thread
+pause
+@echo on
+Debug\Client.exe 1 1 SearchString "hello world"
+@echo off
+pause
+echo Going to search for the string "hello world" using Client.exe using 2 threads
+pause
+@echo on
+Debug\Client.exe 1 2 SearchString "hello world"
+@echo off
+pause
+echo Going to search for the string "hello crazy world" using Client.exe using 2 threads with 2 iterations
+pause
+@echo on
+Debug\Client.exe 2 2 SearchString "hello crazy world"
+@echo off
+pause
+
+
 echo Now going execute Tester application with 2 processes
 pause
 @echo on
